9b052cf03c6e2ed6ea5d1f0a754c022dbafbc8f5
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2023-06-08 21:18:55 +0700

    add activity webview

---
+++ app/src/main/java/com/nikolaswidad/oaseapp/ui/screen/detail/NewsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.nikolaswidad.oaseapp.ui.screen.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nikolaswidad.oaseapp.R
+
+class NewsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+    }
+}
